de6b47886aec43d0adf5868e74226ca6561399bc
amandagpearce <amandagpearce@gmail.com>
2022-08-18 18:09:45 -0300

    using fetch to make an http get request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch('https://swapi.dev/api/films', 
+       // as a second arg could be an obj you can pass headers, methods, body etc
+      // by default a GET request will be sent
+      
+    ).then( // fetch returns a promise
+        response => {
+          return response.json();
+        }).then( // this block executes after the previous then return
+          data => {
+            
+            const transformedData = data.results.map(movieData => { // creating a new obj to rename the keys in the obj
+              return {
+                id: movieData.id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date
+              };
+            });
+            setMovies(transformedData);
+          }
+        );
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
