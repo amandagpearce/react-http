510c1fefc56a99616db50a99459174eb7ed58f80
amandagpearce <amandagpearce@gmail.com>
2022-08-18 18:13:07 -0300

    refactoring fecth with async await

--- src/App.js
+++ src/App.js
@@ -7,30 +7,21 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films', 
-       // as a second arg could be an obj you can pass headers, methods, body etc
-      // by default a GET request will be sent
-      
-    ).then( // fetch returns a promise
-        response => {
-          return response.json();
-        }).then( // this block executes after the previous then return
-          data => {
-            
-            const transformedData = data.results.map(movieData => { // creating a new obj to rename the keys in the obj
-              return {
-                id: movieData.id,
-                title: movieData.title,
-                openingText: movieData.opening_crawl,
-                releaseDate: movieData.release_date
-              };
-            });
-            setMovies(transformedData);
-          }
-        );
+  async function fetchMoviesHandler() {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+ 
+    const transformedData = data.results.map(movieData => { // creating a new obj to rename the keys in the obj
+      return {
+        id: movieData.id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+    setMovies(transformedData);
   }
-
+      
   return (
     <React.Fragment>
       <section>
